1cc2f64658f76b3159ae529022297fb1d6b9c2af
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-22 11:25:52 -0300

    file.py add

---
+++ .github/Test/CheckProfile.py
@@ -0,0 +1,10 @@
+import unittest
+
+class CheckProfile(unittest.TestCase):
+    
+    def Test_Nothing_result(self):
+        # This script was made for test codeql-analysis.yml pass!
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
