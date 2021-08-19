e132beb7d976081106d50406103a05507cc5f692
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-19 11:07:24 -0300

    a empty test add

--- Test/CheckProfile.py
+++ Test/CheckProfile.py
@@ -6,5 +6,9 @@ class CheckProfile(unittest.TestCase):
         # This script was made for test codeql-analysis.yml pass!
         pass
 
+    def infos(self):
+        # something should be do now!
+        pass
+
 if __name__ == "__main__":
     unittest.main()
