e68d132c53b7b3eac688652db036ce0d9ac7d982
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-04 10:36:03 -0300

    autgit.sh add | Minha Especializacao -> add : README_jp.md

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git pull
 git add .
-git commit -m "autgit.sh add | Minha Especializacao -> add : README_ind.md"
+git commit -m "autgit.sh add | Minha Especializacao -> add : README_jp.md"
 git push
 git status
 clear
