24322cff8b5de71024d43ab9aa9e82e7740c8e1b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-20 12:02:43 -0300

    Curriculo_Professional em .txt e .md -> correcoes (N.3) | Curriculo_Professional.pdf -> add

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git pull
 git add -A
-git commit -m "Curriculo_Professional em .txt e .md -> correcoes (N.3)"
+git commit -m "Curriculo_Professional em .txt e .md -> correcoes (N.3) | Curriculo_Professional.pdf -> add"
 git push
 git status
 clear
