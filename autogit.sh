1ff1e1316fb6a444645c5b1cab0222dc38587049
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-28 10:44:54 -0300

    Most Used Languages -> alt num. lps add IN readme ENG/ESP/IND

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git pull
 git add -A
-git commit -m "Most Used Languages -> alt num. lps add"
+git commit -m "Most Used Languages -> alt num. lps add IN readme ENG/ESP/IND"
 git push
 git status
 clear
