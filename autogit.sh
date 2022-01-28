2455aae629783c7d04bd0db3cb957e6a6624f866
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-28 11:14:56 -0300

    Curriculo_Professional.txt & Curriculo_Professional.pdf -> link to LinkedIn : updated

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git pull
 git add -A
-git commit -m "Emoki: ✌️🤓 -> add in READMEs Intro"
+git commit -m "Curriculo_Professional.txt & Curriculo_Professional.pdf -> link to LinkedIn : updated"
 git push
 git status
 clear
