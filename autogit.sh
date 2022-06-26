35f9f9e9b3de5dc605bc3e6609d03de324d96dea
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-06-26 17:34:15 -0300

    Minha Especialização -> alt in README_ita | checkProfile.ts -> added

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git pull
 git add -A
-git commit -m "Minha Especialização -> alt in README_ind"
+git commit -m "Minha Especialização -> alt in README_ita | checkProfile.ts -> added"
 git push
 git status
 exit
